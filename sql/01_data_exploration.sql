037b0ea3fa352e61522a16c2d740b2745a5ba865
Sarah-crtl <Sarahkern0528@gmail.com>
2026-09-14 00:24:05 -0400

    Update SQL queries for data exploration

--- sql/01_data_exploration.sql
+++ sql/01_data_exploration.sql
@@ -1,16 +1,13 @@
 -- Supply Chain GHG Analysis
 -- Data exploration
 
--- View the first 20 rows
 SELECT *
 FROM greenhouse-emissions.supply_chain_ghg.emissions_factors
 LIMIT 20;
 
--- Count the total number of records
 SELECT COUNT(*) AS total_rows
 FROM greenhouse-emissions.supply_chain_ghg.emissions_factors;
 
--- List the industries
 SELECT DISTINCT
   `2017 NAICS Title`
 FROM greenhouse-emissions.supply_chain_ghg.emissions_factors
